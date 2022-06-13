4a0a6c64f283a95968207dbec28d7d4e6c01e947
Jithin Johnson <jithu7432@gmail.com>
2022-06-13 10:23:27 +0530

    added X to close

--- host/server.py
+++ host/server.py
@@ -6,7 +6,6 @@ import socketserver
 import sys
 import threading
 import tkinter
-from turtle import colormode
 from urllib.parse import quote
 
 import pyqrcode
@@ -15,18 +14,24 @@ import pyqrcode
 class QRwindow(tkinter.Tk):
     def __init__(self):
         super().__init__()
-        # self.geometry("400x450")
         self.title("Scan the QR")
-        self.exit_button = tkinter.Button(self, text="EXIT", command=self.destroy, activebackground='red', activeforeground='white',)
+
+        self.exit_button = tkinter.Button(
+            self,
+            text="PRESS 'X' to EXIT OR CLICK HERE",
+            command=self.destroy,
+            activebackground="red",
+            activeforeground="white",
+        )
         self.exit_button.pack(ipadx=225, ipady=10, side=tkinter.BOTTOM)
+
+    def close_window(self, event) -> None: self.destroy()
 
 
 def hostit(directory):
-
     socketserver.TCPServer.allow_reuse_address = True
 
     class Handler(http.server.SimpleHTTPRequestHandler):
-
         def __init__(self, *args, **kwargs):
             super().__init__(*args, directory=directory, **kwargs)
 
@@ -36,39 +41,32 @@ def hostit(directory):
 
 def main():
     if len(sys.argv) == 1:
-        raise NameError(f"Please provide the path to the file which is to be hosted.")
+        raise Exception(f"Provide the path to the file which is to be hosted!")
 
     if not os.path.isfile(sys.argv[1]):
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), sys.argv[1])
 
     file = sys.argv[1]
-    directory, file = os.path.split(file)
+    directory, filename = os.path.split(file)
 
-    host = socket.gethostname()
-    ipad = socket.gethostbyname(host)
+    ipaddress = socket.gethostbyname(socket.gethostname())
 
-    url = f"http://{ipad}:8010/{quote(file)}"
+    url = f"http://{ipaddress}:8010/{quote(filename)}"
     print(url)
 
-    URL = pyqrcode.create(url)
-    xbm = URL.xbm(scale=8)
-
-
     root = QRwindow()
-
-    bmp = tkinter.BitmapImage(data=xbm)
-    # bmp.config(background="white")
+    bmp = tkinter.BitmapImage(data=pyqrcode.create(url).xbm(scale=8))
 
     label = tkinter.Label(image=bmp)
     label.pack()
 
-    threading.Thread(target=hostit, args=(directory, )).start()
+    host_thread = threading.Thread(target=hostit, args=(directory,))
+    host_thread.daemon=True 
+    host_thread.start()
 
+    root.bind("x", root.close_window)
     root.mainloop()
 
 
 if __name__ == "__main__":
-
-    # DEBUGGING
-    sys.argv = ['', '/home/jithin/temp/ubuntu-18.04.6-desktop-amd64.iso']
     main()
